e5f8b554ae584c6828ecd4157386c0704edc5415
segubharath <bharath5e7@gmail.com>
2019-03-16 18:49:02 +0530

    hi segu

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyDGxH2Layw_DwFX54TfwnI_ZKsPnB3WxcE",
+    authDomain: "gameofthrones-bb5ee.firebaseapp.com",
+    databaseURL: "https://gameofthrones-bb5ee.firebaseio.com",
+    projectId: "gameofthrones-bb5ee",
+    storageBucket: "gameofthrones-bb5ee.appspot.com",
+    messagingSenderId: "485167045155"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,23 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDGxH2Layw_DwFX54TfwnI_ZKsPnB3WxcE",
+    authDomain: "gameofthrones-bb5ee.firebaseapp.com",
+    databaseURL: "https://gameofthrones-bb5ee.firebaseio.com",
+    projectId: "gameofthrones-bb5ee",
+    storageBucket: "gameofthrones-bb5ee.appspot.com",
+    messagingSenderId: "485167045155"
+  }
+};
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
